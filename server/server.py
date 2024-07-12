ac04e615ffc0620a6deb8bef0d5edecd92112e65
eitay <alter1eitai@gmail.com>
2024-07-12 04:04:08 +0300

    working docker-compose

--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import insert
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://CBP:CBP!234@eitayalter.ddns.net/CBP"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
